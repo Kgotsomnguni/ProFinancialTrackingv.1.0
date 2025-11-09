134a4e6d58df3648f696d19c8f25c4bf3ef91191
Kgotso mnguni <kgotsomnguni09@gmail.com>
2025-11-10 00:08:10 +0200

    Day 2: Added Income, Expense, and Transaction models

---
+++ Models/Income.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProFinancialTrackingv._1._0.Models
+{
+    public class Income
+    {
+        public string Source { get; set; }  // where the money comes from
+        public decimal Amount { get; set; } // how much
+        public DateTime DateRecieved { get; set; } // when it was recieved
+    }
+}
---
+++ Models/Transaction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProFinancialTrackingv._1._0.Models
+{
+    internal class Transaction
+    {
+    }
+}
